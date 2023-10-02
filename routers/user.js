8d13ebb0f542a0e94ab395ce425b973f99eae916
Carlos Chirinos <carchirinosg@gmail.com>
2023-10-01 20:25:31 -0600

    Saving before updating from repo

--- routers/user.js
+++ routers/user.js
@@ -10,7 +10,7 @@ const {
 } = require('../controllers/user.js');
 
 // Path inicial http://my-app.com/users
-router.post('/', signup);
+router.post('/', signup); 
 router.get('/', getAll);
 router.get('/:id', getById);
 router.get('/email/:email', getByEmail);
